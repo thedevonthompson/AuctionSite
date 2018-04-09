127d305ef480a353916272920307bd4ebd612b80
Devon Thompson <thedevonthompson@gmail.com>
2018-04-09 13:31:47 -0700

    Add project files.

---
+++ AuctionSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionSite.Startup))]
+namespace AuctionSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
