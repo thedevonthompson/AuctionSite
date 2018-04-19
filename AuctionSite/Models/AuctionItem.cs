1536a0ecbce47cdd01fb90081d2a1c8ceb605f5e
thedevonthompson <thedevonthompson@users.noreply.github.com>
2018-04-18 21:17:19 -0700

    Created AuctionItems class, and Bids class that facilitates a many to many relationship between users and auction items.

---
+++ AuctionSite/Models/AuctionItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuctionSite.Models
+{
+    public class AuctionItem
+    {
+        public int AuctionItemID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal MinPrice { get; set; }
+
+        public ICollection<Bid> Bids { get; set; }
+    }
+}
