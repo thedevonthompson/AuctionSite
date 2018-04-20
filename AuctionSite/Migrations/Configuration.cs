20ac50f3a6d4d1eac648400ab7a87af9b270059f
thedevonthompson <thedevonthompson@users.noreply.github.com>
2018-04-20 12:15:11 -0700

    Database seeded with three roles and a test user for each role.

--- AuctionSite/Migrations/Configuration.cs
+++ AuctionSite/Migrations/Configuration.cs
@@ -1,9 +1,13 @@
 namespace AuctionSite.Migrations
 {
+    using AuctionSite.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Web.Configuration;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AuctionSite.Models.ApplicationDbContext>
     {
@@ -26,6 +30,38 @@ namespace AuctionSite.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            CreateRoles(context, RoleActions.Admin, RoleActions.Moderator, RoleActions.Member);
+            CreateUser(context, RoleActions.Admin, WebConfigurationManager.AppSettings["DefaultAdminUsername"], WebConfigurationManager.AppSettings["DefaultAdminPassword"]);
+            CreateUser(context, RoleActions.Moderator, WebConfigurationManager.AppSettings["DefaultModeratorUsername"], WebConfigurationManager.AppSettings["DefaultModeratorPassword"]);
+            CreateUser(context, RoleActions.Member, WebConfigurationManager.AppSettings["DefaultMemberUsername"], WebConfigurationManager.AppSettings["DefaultMemberPassword"]);
+        }
+
+        private void CreateRoles(ApplicationDbContext db, params string[] roles)
+        {
+            var roleStore = new RoleStore<IdentityRole>(db);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            foreach (var role in roles)
+            {
+                if (!db.Roles.Any(r => r.Name == role))
+                {
+                    var identityRole = new IdentityRole { Name = role };
+                    roleManager.Create(identityRole);
+                }
+            }
+        }
+
+        private void CreateUser(ApplicationDbContext db, string role, string username, string password)
+        {
+            if (!db.Users.Any(u => u.UserName == username))
+            {
+                var userStore = new UserStore<ApplicationUser>(db);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var applicationUser = new ApplicationUser { UserName = username, Email = username + "@email.com", EmailConfirmed = true };
+                userManager.Create(applicationUser, password);
+                userManager.AddToRole(applicationUser.Id, role);
+            }
         }
     }
 }
---
+++ AuctionSite/Models/RoleActions.cs
@@ -0,0 +1,9 @@
+﻿namespace AuctionSite.Models
+{
+    public class RoleActions
+    {
+        public const string Admin = "Admin";
+        public const string Moderator = "Mod";
+        public const string Member = "Member";
+    }
+}
